df56ff2b23ecfd56ba303ec58230862021ae31bb
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-09-25 07:27:41 -0300

    Criando Primeiro Microservice

---
+++ microservice-insert/src/test/java/com/talissonmelo/insert/InsertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.insert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
