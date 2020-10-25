eaa546b863e436d3d74a6ed125531a38ed14f47e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-10-25 11:32:08 -0300

    Registando microservico no eureka-service

--- microservice-order/src/main/java/com/talissonmelo/microserviceorder/MicroserviceOrderApplication.java
+++ microservice-order/src/main/java/com/talissonmelo/microserviceorder/MicroserviceOrderApplication.java
@@ -2,7 +2,9 @@ package com.talissonmelo.microserviceorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviceOrderApplication {
 
