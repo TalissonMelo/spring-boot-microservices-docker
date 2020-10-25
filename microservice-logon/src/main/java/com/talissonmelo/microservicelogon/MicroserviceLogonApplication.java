699022765612db2badd767d288ac8e76131a6e18
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-10-25 12:18:59 -0300

    Microserviço de Login

---
+++ microservice-logon/src/main/java/com/talissonmelo/microservicelogon/MicroserviceLogonApplication.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.microservicelogon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceLogonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceLogonApplication.class, args);
+	}
+
+}
---
+++ microservice-logon/src/test/java/com/talissonmelo/microservicelogon/MicroserviceLogonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.microservicelogon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceLogonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
