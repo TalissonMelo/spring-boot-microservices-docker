4ac452437cf3c8b10bcd5c2e745cb1c2dabacd83
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-10-25 11:48:56 -0300

    Criando API Gateway

---
+++ zull-gateway/src/main/java/com/talissonmelo/ZullGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.talissonmelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZullGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZullGatewayApplication.class, args);
+	}
+
+}
